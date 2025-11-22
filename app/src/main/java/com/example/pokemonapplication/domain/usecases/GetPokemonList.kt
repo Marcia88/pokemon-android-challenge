92c6da30929cd502a65bdccbd7e0ed685bb43000
Marcia88 <marciaggp88@gmail.com>
2025-11-22 13:23:08 -0300

    pagination, 20 items, error and loading view

--- app/src/main/java/com/example/pokemonapplication/domain/usecases/GetPokemonList.kt
+++ app/src/main/java/com/example/pokemonapplication/domain/usecases/GetPokemonList.kt
@@ -4,6 +4,8 @@ import com.example.pokemonapplication.domain.model.PokemonListModel
 import kotlin.Result
 import kotlinx.coroutines.flow.Flow
 
+const val ITEMS_PER_PAGE = 20
+
 interface GetPokemonList {
-    fun getPokemonList(): Flow<Result<PokemonListModel>>
+    fun getPokemonList(limit: Int = ITEMS_PER_PAGE, offset: Int = 0): Flow<Result<PokemonListModel>>
 }
